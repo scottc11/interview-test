f13a0e24e726bcd9171bf82bc202c9ff85a46f2c
scottc11 <scott.m.campbell11@gmail.com>
2022-05-17 16:19:04 -0400

    css bundler

--- 02_front/src/index.js
+++ 02_front/src/index.js
@@ -1,8 +1,11 @@
+import './styles.css';
+
 function component() {
     const element = document.createElement('div');
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = "Hello Webpack";
+    element.classList.add("app");
 
     return element;
 }

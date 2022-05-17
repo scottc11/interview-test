8a36257a99c4f1ba2e6eba24086aa86808782f98
scottc11 <scott.m.campbell11@gmail.com>
2022-05-17 13:12:20 -0400

    basic coverage of python class

--- 01_unit_test_python/test_contractor.py
+++ 01_unit_test_python/test_contractor.py
@@ -41,3 +41,31 @@ class TestContractor(unittest.TestCase):
 
     def test_get_name(self):
         self.assertEqual('Erico', self.contractor.get_name())
+
+    def test_get_is_active(self):
+        self.contractor.is_active = True
+        self.assertTrue(self.contractor.get_is_active())
+        self.contractor.is_active = False
+        self.assertFalse(self.contractor.get_is_active())
+
+    def test_set_number_of_completed_project(self):
+        self.contractor.set_number_of_completed_project(7)
+        self.assertEqual(7, self.contractor.number_of_completed_project)
+
+    def test_set_number_of_on_going_project(self):
+        self.contractor.set_number_of_on_going_project(13)
+        self.assertEqual(13, self.contractor.number_of_on_going_project)
+
+    def test_get_total_number_of_project(self):
+        self.contractor.set_number_of_completed_project(3)
+        self.contractor.set_number_of_on_going_project(5)
+        self.assertIs(type(1), type(self.contractor.get_total_number_of_project()), "returned object is not of type 'int'")
+        self.assertEqual(3+5, self.contractor.get_total_number_of_project())
+
+    def test_set_percentage_service_fee_rate(self):
+        self.contractor.set_percentage_service_fee_rate(7)
+        self.assertEqual(7, self.contractor.percentage_service_fee_rate)
+
+    def test_set_total_amount_of_money_earned(self):
+        self.contractor.set_total_amount_of_money_earned(1240)
+        self.assertEqual(1240, self.contractor.total_amount_of_money_earned)
